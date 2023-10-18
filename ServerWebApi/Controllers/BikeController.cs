408c72128ee6e6c0ec6ae6635882c23a11e7f745
avishgah <138613565+avishgah@users.noreply.github.com>
2023-10-18 15:11:54 +0300

    work

---
+++ ServerWebApi/Controllers/BikeController.cs
@@ -0,0 +1,57 @@
+﻿using Bll;
+using Dto;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ServerWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BikeController : ControllerBase
+    {
+        IBikeBll bikeBll;
+        public BikeController(IBikeBll bikeBll)
+        {
+            this.bikeBll = bikeBll;
+        }
+        //get-שליפה
+        // GET: api/<BikeController1>
+        [HttpGet]
+        public List<BikeDto> Get()
+        {
+          return bikeBll.GetBikeList();
+        }
+
+        // GET api/<BikeController1>/5
+        [HttpGet("{id}")]
+        public BikeDto Get(int id)
+        {
+            return bikeBll.GetBike(id);
+        }
+        //post-הוספה
+        // POST api/<BikeController1>
+        [HttpPost]
+        public void Post([FromBody] BikeDto b)
+        {
+            b.DateStart = DateTime.Now;
+            b.Battery = 100;
+            b.Code = "bike" + b.Code;
+            bikeBll.AddBike(b);
+        }
+        //put-עדכון
+        // PUT api/<BikeController1>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] BikeDto b)
+        {
+            bikeBll.UpdateBike(b,id);
+        }
+        //delete-מחיקה
+        // DELETE api/<BikeController1>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            bikeBll.DeleteBike(id);
+        }
+    }
+}
---
+++ ServerWebApi/Controllers/StationController.cs
@@ -0,0 +1,55 @@
+﻿//using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Bll;
+using Dto;
+
+namespace ServerWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StationController : ControllerBase
+    {
+
+        IStattionBll StationBll;
+        public StationController(IStattionBll StationBll)
+        {
+            this.StationBll = StationBll;
+        }
+        //get-שליפה
+        // GET: api/<StationController1>
+        [HttpGet]
+        public List<StationDto> Get()
+        {
+            return StationBll.GetStationList();
+        }
+
+        // GET api/<StationController1>/5
+        [HttpGet("{id}")]
+        public StationDto Get(int id)
+        {
+            return StationBll.GetStation(id);
+        }
+        //post-הוספה
+        // POST api/<StationController1>
+        [HttpPost]
+        public void Post([FromBody] StationDto b)
+        {
+           
+            StationBll.AddStation(b);
+        }
+        //put-עדכון
+        // PUT api/<BikeController1>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] StationDto b)
+        {
+            StationBll.UpdateStation(b, id);
+        }
+        //delete-מחיקה
+        // DELETE api/<BikeController1>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            StationBll.DeleteStation(id);
+        }
+    }
+}
