2e7a1fed61255c86628dfc7e902bd743d094b9b1
avishgah <138613565+avishgah@users.noreply.github.com>
2023-12-12 18:13:07 +0200

    ,,

--- ServerWebApi/Controllers/StationViewControllers.cs
+++ ServerWebApi/Controllers/StationViewControllers.cs
@@ -10,46 +10,46 @@ namespace ServerWebApi.Controllers
     [ApiController]
     public class StationViewControllers : ControllerBase
     {
-       //IStationViewDal stationBll;
-       // public StationViewControllers(IStationViewDal bikeBll)
-       // {
-       //     this.stationBll = bikeBll;
-       // }
-       // // GET: api/<StationVController>
-       // [HttpGet]
-       // public List<StationViewDto> Get()
-       // {
-       //     return stationBll.GetStationList();
-       // }
+        IStationViewDal stationBll;
+        public StationViewControllers(IStationViewDal bikeBll)
+        {
+            this.stationBll = bikeBll;
+        }
+        // GET: api/<StationVController>
+        [HttpGet]
+        public List<StationViewDto> Get()
+        {
+            return stationBll.GetStationList();
+        }
 
-       // // GET api/<StationVController>/5
-       // [HttpGet("{id}")]
-       // public StationViewDto Get(int id)
-       // {
-       //     return stationBll.GetStation(id);
-       // }
+        // GET api/<StationVController>/5
+        [HttpGet("{id}")]
+        public StationViewDto Get(int id)
+        {
+            return stationBll.GetStation(id);
+        }
 
-       // // POST api/<StationVController>
-       // [HttpPost]
-       // public void Post([FromBody] StationViewDto b)
-       // {
+        // POST api/<StationVController>
+        [HttpPost]
+        public void Post([FromBody] StationViewDto b)
+        {
 
-       //     stationBll.AddStation(b);
-       // }
+            stationBll.AddStation(b);
+        }
 
-       // // PUT api/<StationVController>/5
-       // [HttpPut("{id}")]
-       // public void Put(int id, [FromBody] StationViewDto b)
-       // {
-       //     stationBll.UpdateStation(b, id);
-       // }
+        // PUT api/<StationVController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] StationViewDto b)
+        {
+            stationBll.UpdateStation(b, id);
+        }
 
-       // // DELETE api/<StationVController>/5
-       // [HttpDelete("{id}")]
-       // public void Delete(int id)
-       // {
-       //     stationBll.DeleteStation(id);
+        // DELETE api/<StationVController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            stationBll.DeleteStation(id);
 
-       // }
+        }
     }
 }
